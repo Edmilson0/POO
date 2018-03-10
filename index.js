6700fcf8145f4bc8019e1b232323f27986c7d6e3
Edmilson0 <37040809+Edmilson0@users.noreply.github.com>
2018-03-10 21:52:52 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// alinea A
+function funcA(){
+    console.log("merda")
+
+    let array_numerico=[7,3,6,12,5]
+   
+   
+    for (let index = 0; index < array_numerico.length; index++) {
+    let soma=parseInt(soma+array_numerico[index]) 
+    }
+
+   
+    //let media=parseInt(soma/array.length) 
+
+    console.log((media) )
+
+}
+
+
