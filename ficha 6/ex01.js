d5d80941d36587edd5a418bfdcfd916ba0aaa106
Iacatona <iacatonasilva@gmail.com>
2018-03-23 16:49:43 +0000

    ficha 6

---
+++ ficha 6/ex01.js
@@ -0,0 +1,34 @@
+
+//colocar todo o codigo aqui
+window.onload=function(){
+let myP=document.getElementById("myP")
+myP.innerHTML="bem vindo a minha pagina"
+
+let myBtn=this.document.getElementById("myBtn")
+
+myBtn.addEventListener("click",function(){
+
+    let txt=document.getElementById("myTxt")
+
+    if (txt.value=="") {
+        myP.innerHTML="bem vindo a minha pagina"
+       
+    }else{
+        myP.innerHTML="bem vindo, "+txt.value
+    }
+  
+})
+}
+
+// criar listeners para o botao todas
+let btnTodas=document.getElementById("btnTodas")
+btnTodas.addEventListener("click",function(){
+
+    let btns=document.getElementsByTagName("input")
+
+    for (let i = 0; i < btns.length;i ++) {
+       btns[i].style.backgroundColor="red"
+    }
+
+
+})
